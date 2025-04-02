0ea8db3ad9df59bcb35debfcd20a415b3b086807
DaniVaic18 <loljose1090@gmail.com>
2025-04-02 15:26:19 -0500

    feat: Se agrego en la barra lateral la redireccion a buscadores

--- src/layout/SideBar.tsx
+++ src/layout/SideBar.tsx
@@ -1,55 +1,20 @@
 import React from "react";
+import { Link } from "wouter";
 
 const SideBar = () => {
   return (
     <div
-      className="sticky top-[60px] h-[calc(100vh_-_60px)] min-w-[264px] overflow-y-auto pb-2"
+      className="sticky top-[60px] flex flex-col h-[calc(100vh_-_60px)] min-w-[264px] overflow-y-auto px-2 py-3"
       // style={{
       //   height: "calc(100vh - 60px)",
       // }}
     >
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
-      <p>rey</p>
+      <Link
+        href={"/debounce"}
+        className={`w-full cursor-pointer rounded-md border px-2 py-1 transition-all`}
+      >
+        Buscadores
+      </Link>
     </div>
   );
 };
